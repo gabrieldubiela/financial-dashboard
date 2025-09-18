c1446caa7b2f1692fd47a566a046cf023a7235d5
gabrieldubiela <165738698+gabrieldubiela@users.noreply.github.com>
2025-09-17 23:18:48 -0300

    15

--- nextjs-dashboard/next.config.ts
+++ nextjs-dashboard/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    ppr: false // Change this value to false
+  }
 };
 
 export default nextConfig;
